7ca94c779a841ac4a2b693f68d5f608238f1f0d3
Juan Manuel Carballo <jm.carb@gmail.com>
2023-06-24 21:56:07 -0600

    agregado ejemplo observable en recurso time

--- src/coap/client.py
+++ src/coap/client.py
@@ -6,9 +6,7 @@ from aiocoap import *
 async def main():
     protocol = await Context.create_client_context()
 
-    await asyncio.sleep(2)
-
-    request = Message(code = GET, uri='coap://localhost/other/block')
+    request = Message(code = GET, uri='coap://localhost/time')
     response = await protocol.request(request).response
 
     print(f"Result: {response.code} {response.payload}")
